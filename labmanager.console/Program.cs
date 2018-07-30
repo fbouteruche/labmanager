2b64d5da58014bd944d481663d84a37b4611da17
François Bouteruche <frbouter@microsoft.com>
2018-07-30 15:18:07 +0200

    Init labmanager solution

---
+++ labmanager.console/Program.cs
@@ -0,0 +1,33 @@
+﻿using labmanager.core;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace labmanager.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string environmentName = System.Environment.GetEnvironmentVariable("DOTNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Uri organizationUri = new Uri(configuration["organization"]);
+            string pat = configuration["pat"];
+            string project = configuration["project"];
+
+            ReleaseManager releaseManager = new ReleaseManager(organizationUri, pat);
+
+            IEnumerable<string> releaseDefinitions = releaseManager.GetReleaseDefinition(project);
+
+
+        }
+    }
+}
